4bb5f2ca0ec8324c14cbfe464919969f12a54255
Trial <mygithublab@126.com>
2018-11-15 23:21:37 +0800

    20181115232137

--- jenkins-container.sh
+++ jenkins-container.sh
@@ -2,6 +2,6 @@
 
 useradd -U -u 1000 jenkins
 
-docker run -itd --name jenkins -p 10001:8080 -p 10002:50000 -v /home/jenkins:/var/jenkins_home --restart=always jenkins
+docker run -itd --name jenkins -p 10001:8080 -p 10002:50000 -v /home/jenkins:/var/jenkins_home --restart=always jenkins:2.60.2
 
 
