0f8d8c09464969b2ecc083736b2121adff309180
mygithublab <mygithublab@126.com>
2019-06-06 08:59:43 +0800

    20190606085943

--- dokuwiki/dokuwiki.sh
+++ dokuwiki/dokuwiki.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+#If you haven't done this before, create a new network for the application:
+
+#docker network create dokuwiki-tier
+
+#Run the dokuwiki in the contatiner:
 docker run -d -p 10005:80 -p 443:443 --name dokuwiki \
   -e DOKUWIKI_USERNAME=dokuwiki \
   -e DOKUWIKI_FULL_NAME=superuser \
-  -e DOKUWIKI_PASSWORD=dokuwiki \
+  -e DOKUWIKI_PASSWORD=cdcmcc \
   -e DOKUWIKI_EMAIL=mygithublab@126.com \
   -e DOKUWIKI_WIKI_NAME=CDC-WIKI \
   --net dokuwiki-tier \
-  --volume /path/to/dokuwiki-persistence:/bitnami/dokuwiki \
+  --volume /root/dokuwiki-persistence:/bitnami/dokuwiki \
   --restart=always \
   bitnami/dokuwiki:latest
+
+
+#Take a snapshot of the appliction state
+
+#rsync -a /root/dokuwiki-persistence /root/dokuwiki-persistence.bkp.$(date +%Y%m%d-%H.%M.%S)
